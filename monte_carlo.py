9ef0a392eb5407cd20a667407ef0ed7ed90d1ece
Roni Horo <rhoro18@student.aau.dk>
2020-11-29 18:17:18 +0100

    added run method

--- monte_carlo.py
+++ monte_carlo.py
@@ -4,9 +4,9 @@ import numpy as np
 import csv
 import math
 import scipy.integrate as si
+import doctest 
 
 class monte_carlo():
-
     time = 46800
     x = []
     time_x = 0
@@ -54,10 +54,9 @@ class monte_carlo():
         u = np.random.uniform(0,1)
         for i in range(len(self.total_a)):
             temp = (self.total_a[i])/(30 * self.total)
-            temp2 = (self.total_a[i])/200
+            temp2 = (self.total_a[i])/self.total_a
             prob_interval.append(temp)
             prob_interval_adjusted.append(temp2)
-            # print("[", self.start[i], self.end[i],"]:", prob_interval[i])
             print("[", self.start[i],":", self.end[i],"]:", prob_interval_adjusted[i])
 
         for i in range(len(prob_list)):
@@ -92,10 +91,6 @@ class monte_carlo():
             elif landing_times[i] == 9:
                 temp = np.random.uniform(241,270)
                 landing_times[i] = int(temp)
-            # if landing_times[i] != 0:
-            #     # print("type",prob_list[i])
-            #     # print("landings time",landing_times[i])
-        
         can_land = True
         que_size = []
         waiting_time = []
@@ -109,9 +104,6 @@ class monte_carlo():
                     self.time_x = i
                     self.time_y = self.time_x + landing_times[i]
                     queue.append(self.time_y)
-                    # if i == queue[0] and len(queue) != 0: 
-                    #     queue.pop(0)
-                    #     que_size.append(len(queue))
                 elif len(queue) != 0:
                     if queue[-1] >= i: 
                         diff = queue[-1] - i
@@ -119,51 +111,27 @@ class monte_carlo():
                         waiting_time.append(wait)
                         q = diff + i + landing_times[i]
                         queue.append(q)
-                        # if i == queue[0] and len(queue) != 0: 
-                        #     queue.pop(0)
-                        #     que_size.append(len(queue))
                     else: 
                         q = landing_times[i]+ i 
                         queue.append(q)
-                        # if i == queue[0] and len(queue) != 0: 
-                        #     queue.pop(0)
-                        #     que_size.append(len(queue))
-                    # y = self.time_x + landing_times[i]
                 
             print("queue length:", len(queue))
         for i in range(len(queue)):
             print ("q", queue[i])
 
-        # for i in range(self.time):
-        #     if landing_times[i] != 0: 
-        #         queue.append(i)
-        #         print(i)
-        #         print(landing_times[i])
-        #         if i + landing_times[i] >= landing_times[i+1]:
-
-
-        # for i in range(len(prob_interval)):
-        #     f = si.quad(func,-np.Infinity,30)
-        #     print (f)
-
-        # for i in range(0, time):
-        #     prob_list.append(pois_dist)
-        #     print ("pois dist: ", prob_list[i])
 
         plt.xlabel("Time")
         plt.ylabel("Probality")
         plt.plot(que_size)
         plt.show()
-        
+
+    def run(self):
+        self.get_total()
+        self.probality_distribution()
+        self.simulation()
 
 
 if __name__ == '__main__':
+    doctest.testmod()
     m = monte_carlo()
-    m.get_total()
-    m.probality_distribution()
-    m.simulation()
-    # m.get_total()
-    # time = 46800 #13 timer 
-    # k = m.get_k(time)
-    # m.calc_poisson()
-    # m.poisson(time)
+    m.run()
